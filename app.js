9d2dad29eda72e5f44858752678926cefcee97a4
anandaandikabp <ananda.andika18@gmail.com>
2022-07-19 15:29:24 +0700

    crud with postgre

--- app.js
+++ app.js
@@ -8,33 +8,20 @@ const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const flash = require('connect-flash');
 const session = require('express-session');
-const multer  = require('multer');
+const multer = require('multer');
 const pool = require('./db');
 const { loadContact, detailContact, addContact, hapusContact, cekDuplikat, updateContact } = require('./utils/contact');
 
-// app.get('/addsync', async (req, res) => {
-//     try {
-//         const name = "Rhaina"
-//         const mobile = "0812345678"
-//         const email = "ananda@mail.com"
-//         const newCont = await pool.query(`INSERT INTO contacts values
-//         ('${name}','${mobile}','${email}') RETURNING *`)
-//         res.json(newCont)
-//     } catch (err) {
-//         console.error(err.message)
-//     }
-// });
-
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, './public/img')
+        cb(null, './public/img')
     },
     filename: (req, file, cb) => {
-      console.log(file)
-      cb(null, Date.now() + path.extname(file.originalname))
+        console.log(file)
+        cb(null, Date.now() + path.extname(file.originalname))
     }
-  })
-  
+})
+
 const upload = multer({ storage: storage })
 
 app.set('view engine', 'ejs');
@@ -55,12 +42,12 @@ app.use(morgan('dev'));
 app.use(cookieParser('secret'))
 app.use(flash());
 
-app.use(session({ 
+app.use(session({
     cookie: { maxAge: 6000 },
     secret: 'secret',
     resave: true,
     saveUninitialized: true,
- }));
+}));
 
 // index
 app.get('/', (req, res, next) => {
@@ -74,7 +61,7 @@ app.get('/', (req, res, next) => {
 
 // about
 app.get('/about', (req, res, next) => {
-    res.render('about', { 
+    res.render('about', {
         title: 'Laman About',
         layout: 'layout/main-layout',
     });
@@ -84,7 +71,7 @@ app.get('/contact', async (req, res) => {
     // const contact = loadContact();
     const query = await pool.query('SELECT * FROM contacts')
     const contact = query.rows;
-    res.render('contact', { 
+    res.render('contact', {
         title: 'Laman Contact',
         layout: 'layout/main-layout',
         contact,
@@ -105,8 +92,8 @@ app.get('/contact/add', (req, res) => {
 app.get('/contact/:name', async (req, res, next) => {
     // const contact = detailContact(req.params.name);
     const query = await pool.query(`SELECT * FROM contacts WHERE name = '${req.params.name}'`)
-    const contact = query.rows [0];
-    res.render('detail', { 
+    const contact = query.rows[0];
+    res.render('detail', {
         title: 'Laman Detail',
         layout: 'layout/main-layout',
         contact,
@@ -118,7 +105,7 @@ app.get('/contact/:name', async (req, res, next) => {
 app.post('/contact', upload.single('image'), [
     body('name').custom(async (value) => {
         const query = await pool.query(`SELECT * FROM contacts WHERE lower(name) = lower('${value}')`)
-        const duplikat = query.rows [0];
+        const duplikat = query.rows[0];
         if (duplikat) {
             throw new Error('Nama sudah digunakan')
         }
@@ -148,7 +135,7 @@ app.post('/contact', upload.single('image'), [
 app.get('/contact/delete/:name', async (req, res) => {
     // const contact = detailContact(req.params.name);
     const query = await pool.query(`SELECT * FROM contacts WHERE name = '${req.params.name}'`)
-    const contact = query.rows [0];
+    const contact = query.rows[0];
     if (!contact) {
         res.status(404);
         res.send('<h1>404</h1>')
@@ -164,7 +151,7 @@ app.get('/contact/delete/:name', async (req, res) => {
 app.get('/contact/edit/:name', async (req, res) => {
     // const contact = detailContact(req.params.name);
     const query = await pool.query(`SELECT * FROM contacts WHERE lower(name) = lower('${req.params.name}')`)
-    const contact = query.rows [0];
+    const contact = query.rows[0];
     res.render('edit-contact', {
         title: 'Laman Edit Contact',
         layout: 'layout/main-layout',
@@ -176,7 +163,7 @@ app.get('/contact/edit/:name', async (req, res) => {
 app.post('/contact/update', [
     body('name').custom(async (value, { req }) => {
         const query = await pool.query(`SELECT * FROM contacts WHERE lower(name) = lower('${value}')`)
-        const duplikat = query.rows [0];
+        const duplikat = query.rows[0];
         if (value !== req.body.oldName && duplikat) {
             throw new Error('Nama sudah digunakan')
         }
@@ -186,7 +173,7 @@ app.post('/contact/update', [
     check('mobile', 'Nomor tidak valid').isMobilePhone('id-ID')
 ], async (req, res) => {
     const errors = validationResult(req);
-    const {  oldName, name, mobile, email } = req.body
+    const { oldName, name, mobile, email } = req.body
     if (!errors.isEmpty()) {
         res.render('edit-contact', {
             title: 'form edit',
